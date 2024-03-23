fac8479ab0355289252aed149af1c96f6ac27512
mohamed skakr <mskakar088@gmail.com>
2024-03-23 23:03:30 +0000

    delete all useless components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ import { InternalErrorComponent } from './components/common/internal-error/inter
 import { NotFoundComponent } from './components/common/not-found/not-found.component';
 import { EcommerceComponent } from './components/dashboard/ecommerce/ecommerce.component';
 import { AccountComponent } from './components/pages/account/account.component';
-import { ConnectionsComponent } from './components/pages/connections/connections.component';
 import { CreateProductComponent } from './components/pages/ecommerce/create-product/create-product.component';
 import { ProductDetailsComponent } from './components/pages/ecommerce/product-details/product-details.component';
 import { ProductSellersComponent } from './components/pages/ecommerce/product-sellers/product-sellers.component';
@@ -22,10 +21,7 @@ import { UsersComponent } from './components/pages/ecommerce/users/users.compone
 import { ProductsOrderDetailsComponent } from './components/pages/ecommerce/products-order-details/products-order-details.component';
 import { ProductsOrdersComponent } from './components/pages/ecommerce/products-orders/products-orders.component';
 import { ProductsComponent } from './components/pages/ecommerce/products/products.component';
-import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
 import { ProfileComponent } from './components/pages/profile/profile.component';
-import { SecurityComponent } from './components/pages/security/security.component';
-import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
 import { authGuardGuard, loginGuard } from './components/core/Guards/Auth-guard.guard';
 import { CategoryComponent } from './components/pages/ecommerce/category/category.component';
 
@@ -50,10 +46,6 @@ const routes: Routes = [
         { path: 'sellers', component: ProductSellersComponent },
         { path: 'profile', component: ProfileComponent},
         { path: 'account', component: AccountComponent},
-        { path: 'security', component: SecurityComponent},
-        { path: 'connections', component: ConnectionsComponent},
-        { path: 'privacy-policy', component: PrivacyPolicyComponent},
-        { path: 'terms-conditions', component: TermsConditionsComponent},
         { path: 'category', component: CategoryComponent},
     ]},
     
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -69,22 +69,10 @@ import { BestSellingProductsComponent } from './components/dashboard/ecommerce/b
 import { EcommerceStatsComponent } from './components/dashboard/ecommerce/ecommerce-stats/ecommerce-stats.component';
 import { AudienceOverviewComponent } from './components/dashboard/ecommerce/audience-overview/audience-overview.component';
 import { EcommerceRatingsComponent } from './components/dashboard/ecommerce/ecommerce-ratings/ecommerce-ratings.component';
-import { EcommerceImpressionsComponent } from './components/dashboard/ecommerce/ecommerce-impressions/ecommerce-impressions.component';
 import { VisitsByDayComponent } from './components/dashboard/ecommerce/visits-by-day/visits-by-day.component';
-import { EcommerceActivityTimelineComponent } from './components/dashboard/ecommerce/ecommerce-activity-timeline/ecommerce-activity-timeline.component';
-import { SalesByLocationsComponent } from './components/dashboard/ecommerce/sales-by-locations/sales-by-locations.component';
 import { NewCustomersComponent } from './components/dashboard/ecommerce/new-customers/new-customers.component';
-import { LiveVisitsOnOurSiteComponent } from './components/dashboard/ecommerce/live-visits-on-our-site/live-visits-on-our-site.component';
 import { RevenueStatusComponent } from './components/dashboard/ecommerce/revenue-status/revenue-status.component';
 
-import { AnalyticsCustomersComponent } from './components/pages/analytics-customers/analytics-customers.component';
-import { AnalyticsReportsComponent } from './components/pages/analytics-reports/analytics-reports.component';
-import { AcAudienceOverviewComponent } from './components/pages/analytics-customers/ac-audience-overview/ac-audience-overview.component';
-import { AcStatusComponent } from './components/pages/analytics-customers/ac-status/ac-status.component';
-import { ArRevenueReportComponent } from './components/pages/analytics-reports/ar-revenue-report/ar-revenue-report.component';
-import { ArAverageReportComponent } from './components/pages/analytics-reports/ar-average-report/ar-average-report.component';
-import { ArSessionsComponent } from './components/pages/analytics-reports/ar-sessions/ar-sessions.component';
-import { ArBrowserUsedTrafficReportsComponent } from './components/pages/analytics-reports/ar-browser-used-traffic-reports/ar-browser-used-traffic-reports.component';
 import { ProductsComponent } from './components/pages/ecommerce/products/products.component';
 import { ProductDetailsComponent } from './components/pages/ecommerce/product-details/product-details.component';
 import { CreateProductComponent } from './components/pages/ecommerce/create-product/create-product.component';
@@ -94,20 +82,9 @@ import { UsersComponent } from './components/pages/ecommerce/users/users.compone
 import { ProductsCartComponent } from './components/pages/ecommerce/products-cart/products-cart.component';
 import { ProductsCheckoutComponent } from './components/pages/ecommerce/products-checkout/products-checkout.component';
 import { ProductSellersComponent } from './components/pages/ecommerce/product-sellers/product-sellers.component';
-import { FlaticonComponent } from './components/pages/icons/flaticon/flaticon.component';
 
-import { MapsComponent } from './components/pages/maps/maps.component';
 import { ProfileComponent } from './components/pages/profile/profile.component';
 import { AccountComponent } from './components/pages/account/account.component';
-import { SecurityComponent } from './components/pages/security/security.component';
-import { ConnectionsComponent } from './components/pages/connections/connections.component';
-import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
-import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
-import { PricingComponent } from './components/pages/pricing/pricing.component';
-import { FaqComponent } from './components/pages/faq/faq.component';
-import { GalleryComponent } from './components/pages/gallery/gallery.component';
-import { NotificationsComponent } from './components/pages/notifications/notifications.component';
-import { SearchComponent } from './components/pages/search/search.component';
 import { NotFoundComponent } from './components/common/not-found/not-found.component';
 import { InternalErrorComponent } from './components/common/internal-error/internal-error.component';
 import { ResetPasswordComponent } from './components/authentication/reset-password/reset-password.component';
@@ -126,7 +103,6 @@ import { EditCreateDialogComponent } from './components/shared/components/edit-c
 import { CategoryDialogComponent } from './components/shared/components/category-dialog/category-dialog.component';
 import { httpInterceptorProviders } from './components/core/interceptors/auth.interceptor';
 import { ChangeRoleComponent } from './components/shared/components/change-role/change-role.component';
-import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
 import { RoleTransformPipe } from './components/core/pipes/role-transform.pipe';
 import { SnackBarComponent } from './components/shared/components/snack-bar/snack-bar.component';
 import { OrderDialogComponent } from './components/shared/components/order-dialog/order-dialog.component';
@@ -145,22 +121,10 @@ import { OrderDialogComponent } from './components/shared/components/order-dialo
         EcommerceStatsComponent,
         AudienceOverviewComponent,
         EcommerceRatingsComponent,
-        EcommerceImpressionsComponent,
         VisitsByDayComponent,
-        EcommerceActivityTimelineComponent,
-        SalesByLocationsComponent,
         NewCustomersComponent,
-        LiveVisitsOnOurSiteComponent,
         RevenueStatusComponent,
         RoleTransformPipe,
-        AnalyticsCustomersComponent,
-        AnalyticsReportsComponent,
-        AcAudienceOverviewComponent,
-        AcStatusComponent,
-        ArRevenueReportComponent,
-        ArAverageReportComponent,
-        ArSessionsComponent,
-        ArBrowserUsedTrafficReportsComponent,
         ProductsComponent,
         ProductDetailsComponent,
         CreateProductComponent,
@@ -170,21 +134,8 @@ import { OrderDialogComponent } from './components/shared/components/order-dialo
         ProductsCartComponent,
         ProductsCheckoutComponent,
         ProductSellersComponent,
-        FlaticonComponent,
-        MapsComponent,
         ProfileComponent,
         AccountComponent,
-        SecurityComponent,
-        ConnectionsComponent,
-        PrivacyPolicyComponent,
-        TermsConditionsComponent,
-        PricingComponent,
-        FaqComponent,
-        GalleryComponent,
-        NotificationsComponent,
-        TestimonialsComponent,
-        SearchComponent,
-    
         NotFoundComponent,
         InternalErrorComponent,
         ResetPasswordComponent,
